a15e5f661d63ef935c9c5125acdd2ebb4aa1e0a9
sandeepchhapola <schhapola0001@gmail.com>
2015-09-04 14:03:50 +0530

    Added DataStore service, enums in content section, Saved info data to dataStore

--- control/content/controllers/content.home.controller.js
+++ control/content/controllers/content.home.controller.js
@@ -3,8 +3,22 @@
 (function (angular) {
   angular
     .module('mediaCenterRSSPluginContent')
-    .controller('ContentHomeCtrl', ['$scope',
-      function ($scope) {
+    .controller('ContentHomeCtrl', ['$scope', 'DataStore', 'TAG_NAMES', 'LAYOUTS',
+      function ($scope, DataStore, TAG_NAMES, LAYOUTS) {
+        var _data = {
+          "content": {
+            "carouselImages": [],
+            "description": "<p>&nbsp;<br></p>",
+            "rssUrl": ""
+          },
+          "design": {
+            "itemListLayout": LAYOUTS.itemListLayouts[0].name,
+            "itemDetailsLayout": LAYOUTS.itemDetailsLayouts[0].name,
+            "itemListBgImage": "",
+            "itemDetailsBgImage": ""
+          }
+        };
+
         var ContentHome = this;
         ContentHome.isInValidUrl = false;
         ContentHome.isValidUrl = false;
@@ -14,7 +28,77 @@
           trusted: true,
           theme: 'modern'
         };
-        ContentHome.description='Hi! how are you?';
+        ContentHome.data = angular.copy(_data);
+        ContentHome.masterData = null;
+        updateMasterItem(_data);
+
+        function updateMasterItem(data) {
+          ContentHome.masterData = angular.copy(data);
+        }
+
+        function isUnchanged(data) {
+          return angular.equals(data, ContentHome.masterData);
+        }
+
+
+        /*
+         * Go pull any previously saved data
+         * */
+        var init = function () {
+          var success = function (result) {
+              console.info('Init success result:', result);
+              ContentHome.data = result.data;
+              updateMasterItem(ContentHome.data);
+              if (tmrDelay)clearTimeout(tmrDelay);
+            }
+            , error = function (err) {
+              console.error('Error while getting data', err);
+              if (tmrDelay)clearTimeout(tmrDelay);
+            };
+          DataStore.get(TAG_NAMES.RSS_FEED_INFO).then(success, error);
+        };
+        init();
+
+        /*
+         * Call the datastore to save the data object
+         */
+        var saveData = function (newObj, tag) {
+          if (typeof newObj === 'undefined') {
+            return;
+          }
+          var success = function (result) {
+              console.info('Saved data result: ', result);
+              updateMasterItem(newObj);
+            }
+            , error = function (err) {
+              console.error('Error while saving data : ', err);
+            };
+          DataStore.save(newObj, tag).then(success, error);
+        };
+
+        /*
+         * create an artificial delay so api isnt called on every character entered
+         * */
+        var tmrDelay = null;
+        var saveDataWithDelay = function (newObj) {
+          if (newObj) {
+            if (isUnchanged(newObj) && newObj.content.description !== '<p><br data-mce-bogus="1"></p>') {
+              return;
+            }
+            if (tmrDelay) {
+              clearTimeout(tmrDelay);
+            }
+            tmrDelay = setTimeout(function () {
+              saveData(JSON.parse(angular.toJson(newObj)), TAG_NAMES.RSS_FEED_INFO);
+            }, 500);
+          }
+        };
+        /*
+         * Watch for changes in data and trigger the saveDataWithDelay function on change
+         * */
+        $scope.$watch(function () {
+          return ContentHome.data;
+        }, saveDataWithDelay, true);
 
       }]);
 })(window.angular);
