78c923ac4c57e836c2de23d5dbd2b0f4b3c79017
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-02-19 12:53:15 +0530

    Added condition for layout 2 as well

--- widget/controllers/widget.home.controller.js
+++ widget/controllers/widget.home.controller.js
@@ -222,7 +222,12 @@
          * @returns {boolean}
          */
         WidgetHome.showDescription = function (description) {
-          return !((description == '<p>&nbsp;<br></p>') || (description == '<p><br data-mce-bogus="1"></p>'));
+          var _retVal = false;
+          description = description.trim();
+          if(description && (description !== '<p>&nbsp;<br></p>') && (description !== '<p><br data-mce-bogus="1"></p>')) {
+            _retVal = true;
+          }
+          return _retVal;
         };
 
         /**
