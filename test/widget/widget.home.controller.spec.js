310cf3a6494728111a54b8cf784c1579c7d371ed
vineetasharma <vineetasharma18791@gmail.com>
2016-03-02 16:40:48 +0530

    Test cases fixes for onRefresh method

--- test/widget/widget.home.controller.spec.js
+++ test/widget/widget.home.controller.spec.js
@@ -16,7 +16,14 @@ describe('Unit : pluginPluginMediaCenterRSS widget.home.controller.js', function
     Location = _Location_;
     TAG_NAMES = _TAG_NAMES_;
     Buildfire = {
-      spinner: {}
+      spinner: {},
+      datastore: {
+        onRefresh: function () {
+          return{
+            clear:function(){}
+          }
+        }
+      }
     };
     Buildfire.spinner = jasmine.createSpyObj('Buildfire.spinner', ['show', 'hide']);
     Buildfire.spinner.show.and.callFake(function () {
@@ -33,7 +40,7 @@ describe('Unit : pluginPluginMediaCenterRSS widget.home.controller.js', function
       return deferred.promise;
     });
     DataStore = {};
-    DataStore = jasmine.createSpyObj('DataStore', ['get', 'onUpdate', 'clearListener']);
+    DataStore = jasmine.createSpyObj('DataStore', ['get', 'onUpdate', 'clearListener','onRefresh']);
     ItemDetailsService = {};
     ItemDetailsService = jasmine.createSpyObj('ItemDetailsService', ['getData', 'setData']);
     ItemDetailsService.setData.and.callFake(function (data) {
