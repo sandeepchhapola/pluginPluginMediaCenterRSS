1dd0f17a2aa4e2b038637d062abf5f3b7f9d621e
vineetasharma <vineetasharma18791@gmail.com>
2016-03-07 14:53:50 +0530

    Back button implemented for 'nowplaying' route and replaced the 'navigateHome' with '_goBackOne'

--- widget/app.js
+++ widget/app.js
@@ -37,7 +37,7 @@
                     controller: 'WidgetMediaCtrl'
                 })
                 .when('/nowplaying', {
-                    templateUrl: 'templates/now-playing.html',
+                    templateUrl: 'templates/now-playing1.html',
                     controllerAs: 'NowPlaying',
                     controller: 'NowPlayingCtrl'
                 })
@@ -48,12 +48,17 @@
         .run(['Location', '$location', '$rootScope', function (Location, $location, $rootScope) {
             buildfire.navigation.onBackButtonClick = function () {
                 var reg = /^\/item/;
+                var reg1 = /^\/nowplaying/;
                 if (reg.test($location.path())) {
                     $rootScope.showFeed = true;
                     Location.goTo('#/');
                 }
+                if (reg1.test($location.path())) {
+                    $rootScope.showFeed = false;
+                    Location.goTo('#/item');
+                }
                 else {
-                    buildfire.navigation.navigateHome();
+                    buildfire.navigation._goBackOne();
                 }
             }
         }])
