442acfee2b3259c5d4b0c5fdc47b47e2c10596c5
rishabhdixit <rishabh.dixit@intelligrape.com>
2016-03-14 17:06:12 +0530

    Made required changes for pivotal issue: #115494479

--- widget/filters.js
+++ widget/filters.js
@@ -51,10 +51,16 @@
    */
     .filter('safeHtml', ['$sce', function ($sce) {
       return function (html) {
-        if (html) {
-          return $sce.trustAsHtml(html);
-        }
-        else {
+          if (html) {
+              var $html = $('<div />', {html: html});
+              $html.find('iframe').each(function (index, element) {
+                  var src = element.src;
+                  console.log('element is: ', src, src.indexOf('http'));
+                  src = src && src.indexOf('file://') != -1 ? src.replace('file://', 'http://') : src;
+                  element.src = src && src.indexOf('http') != -1 ? src : 'http:' + src;
+              });
+              return $sce.trustAsHtml($html.html());
+          } else {
           return "";
         }
       };
