fe6d9933267ae8ab2cfcfe0cb044c113a5d3fcaf
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-04-07 16:21:25 +0530

    Removed old placeholders. Replaced with new ones. Also showed gif until images load

--- widget/app.js
+++ widget/app.js
@@ -77,5 +77,20 @@
                         height: height
                     });
             }
-        }]);
+        }])
+      .directive("loadImage", [function () {
+        return {
+          restrict: 'A',
+          link: function (scope, element, attrs) {
+            element.attr("src", "../../../styles/media/holder-" + attrs.loadImage + ".gif");
+
+            var elem = $("<img>");
+            elem[0].onload = function () {
+              element.attr("src", attrs.finalSrc);
+              elem.remove();
+            };
+            elem.attr("src", attrs.finalSrc);
+          }
+        };
+      }]);
 })(window.angular, window.buildfire);
